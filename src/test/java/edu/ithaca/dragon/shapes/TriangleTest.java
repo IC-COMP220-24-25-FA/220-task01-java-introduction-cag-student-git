e112c764ca90dad8e2090aa0083e745c8cc08d81
CharlesAG <zenheadset2@gmail.com>
2024-09-01 23:41:15 -0400

    Created test class and methods for the Triangle class

--- src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/TriangleTest.java
@@ -7,20 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class TriangleTest() {
+public class TriangleTest {
 
     @Test
-    private void calcAreaTest(){
+    public void calcAreaTest(){
         // Not implemented yet
     }
 
     @Test
-    private void doubleSizeTest(){
+    public void doubleSizeTest(){
         // Not implemented yet
     }
 
     @Test
-    private void longestLineWithinTest(){
+    public void longestLineWithinTest(){
         // Not implemented yet
     }
 }
