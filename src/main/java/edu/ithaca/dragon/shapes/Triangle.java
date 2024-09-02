247ad149bd5584ed83ce387a5f3e57b3e4360c81
CharlesAG <zenheadset2@gmail.com>
2024-09-01 23:30:52 -0400

    Created Triangle class, with corresponding (unimplemented) methods

---
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -0,0 +1,23 @@
+package edu.ithaca.dragon.shapes;
+
+public class Triangle {
+    private double base;
+    private double height;
+    private double triangle_size;
+
+    public Triangle(double triangle_base, double triangle_height){
+        // Not implemented yet
+    }
+
+    public double calcArea(){
+        // Not implemented yet
+    }
+
+    public void doubleSize(){
+        // Not implemented yet
+    }
+
+    public double longestLineWithin(){
+        // Not implemented yet
+    }
+}
