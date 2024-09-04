720b4d0731ac43cad51647763b4a0eedefe70f1e
cpag <cammermangerke@ithaca.edu>
2024-09-03 23:05:32 -0400

    Added comments for the test class of class largestOfThree to describe what is being tested for

--- src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
+++ src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
@@ -12,19 +12,28 @@ public class FunctionPracticeTest {
 
     @Test
     public void largestOfThreeTest(){
+        // Check that the largest of the three values of 5, which is the largest inputted
         assertEquals(5, FunctionPractice.largestOfThree(4, 5, 2));
+        // Same as above, but the largest input (5), is in a different parameter
         assertEquals(5, FunctionPractice.largestOfThree(5, 4, 2));
+        // Same as above, but the largest input (5), is in a different parameter
         assertEquals(5, FunctionPractice.largestOfThree(4, 2, 5));
+        // Check that the program returns the correct value when all inputs are identical
         assertEquals(5, FunctionPractice.largestOfThree(5, 5, 5));
 
+        // Check that the program returns an error when a negative value is inputted in all positions
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(-1, -1, -1));
+        // Check that the program returns an error when a negative value is inputted in at least one position
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, -1, 3));
+        // Check that the program returns an error when a negative value is inputted in at least one position, but in a differnt parameter
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, 3, -1));
     }
 
     @Test
     public void calcSalePriceTest(){
-        fail("Not implemented yet");
+        assertEquals(9.5, FunctionPractice.calcSalePrice(100, 90, 5), 0.0001);
+        assertEquals(0, FunctionPractice.calcSalePrice(10, 100, 8), 0.0001);
+        assertEquals(25, FunctionPractice.calcSalePrice(50, 50, 0), 0.0001);
+        assertEquals(25, FunctionPractice.calcSalePrice(37.5, 0, 25), 0.0001);
     }
-    
 }
